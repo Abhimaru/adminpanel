977b0324d051c50a5db8092a71603305989cb3f5
connectus22 <connectus1111@gmail.com>
2022-09-29 13:33:56 +0530

    Login Page Updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,21 @@
 import "./Login.scss";
+import Button from "../../components/button/Button"
 
 const Login = () => {
   return (
-    <div className="home">
-      <h1 className="title">Login</h1>
+    <div className="main">
+      <div className="login">
+          <div className="top">
+            <span className="title">Admin Login</span>
+          </div>
+          <div className="center">
+            <input type="text" id="username" className="inputbox" />
+            <input type="text" id="password" className="inputbox"/>
+          </div>
+          <div className="bottom">
+            <Button title="Login" className="loginbtn"/>
+          </div>
+      </div>
     </div>
   );
 };
